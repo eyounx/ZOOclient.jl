b6ed0811286ef5b31f48c1eedc4f49a8e06854d5
Yu-Ren Liu <liuyr@lamda.nju.edu.cn>
2018-01-30 15:43:39 +0800

    update

--- src/ZOOclient.jl
+++ src/ZOOclient.jl
@@ -2,31 +2,14 @@ module ZOOclient
 
 using Base.Dates.now
 
-export Dimension, dim_rand_sample, dim_limited_space, dim_print, is_discrete
-export Objective, obj_construct_solution, obj_eval, get_history_bestsofar
-export zoo_min, exp_min
-export Parameter, autoset!
-export Solution, find_max, find_min, sol_print, sol_equal
+export Dimension, dim_print
+export Objective, get_history_bestsofar
+export zoo_min
+export Parameter
+export Solution, sol_print, sol_equal
 
-export zoolog, rand_uniform, convert_time, mydistance
+export zoolog
 export rng, my_precision, set_seed, set_precision
-
-export RacosClassification, mixed_classification, rand_sample
-# racos_common.jl
-export RacosCommon, rc_clear!, init_attribute!, selection!, distinct_sample,
-distinct_sample_classifier, print_positive_data, print_negative_data, print_data,
-distinct_sample_from_set
-# racos_optimization.jl
-export RacosOptimization, opt!, ro_clear!, set_ub
-# racos.jl
-export Racos, racos_opt!
-# sracos.jl
-export SRacos, sracos_opt!, sracos_replace!
-
-export ASRacos, asracos_init_sample_set!
-
-export asracos_opt!
-export pposs_opt!
 
 include("ZOOclient/utils/tool_function.jl")
 include("ZOOclient/utils/zoo_global.jl")
